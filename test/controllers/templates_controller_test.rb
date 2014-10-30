6a4a82ca5cc54a4f09932575158d207de0e3e952
jcharb03 <charbon4@uwm.edu>
2014-10-30 17:55:07 -0500

    Adding template tests, and updated Gemfile

--- test/controllers/templates_controller_test.rb
+++ test/controllers/templates_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class TemplatesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  #test template with no params gives 404 error
+  test "test root template" do
+    get(:root, {'template' => ""})
+    assert_response :missing
+  end
+
+  #test template with non empty param gives 200 status code with non empty page
+  test "test library template" do
+    get(:root, {'template' => "library"})
+    assert_response :success
+    assert_template :library
+  end
+  test "test media template" do
+    get(:root, {'template' => "media"})
+    assert_response :success
+    assert_template :media
+  end
+  test "test menu" do
+    get(:root, {'template' => "menu"})
+    assert_response :success
+    assert_template :menu
+  end
 end
