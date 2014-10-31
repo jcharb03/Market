cebfdc2fff2cfb65dc93cabfebce8f89eed252fb
Maximilian <felgenh3@gmail.com>
2014-10-31 17:05:26 -0500

    Namespaced the library

--- app/assets/javascripts/library.js
+++ app/assets/javascripts/library.js
@@ -1,24 +1,24 @@
+var Market = (function (Market) {
+    Market.Views = Market.Views || {};
 
-var LibraryView = Backbone.View.extend({
-    el:"#content",
-    initialize: function () {
-	this.template = _.template($("#library-template").html());
-	this.render();
-    },
-    render: function() {
-	var data = [1, 2, 3, 4];
-	this.$el.html(this.template({data: data}));
-	return this;
-    }
-});
-
-
-
-templateLoader.on("load:templates", function () {
-
-    
-
-    new LibraryView();
-});
+    Market.Views.LibraryView = Backbone.View.extend({
+	el:"#content",
+	initialize: function () {
+	    this.template = _.template($("#library-template").html());
+	    this.render();
+	},
+	render: function() {
+	    var data = [1, 2, 3, 4];
+	    this.$el.html(this.template({data: data}));
+	    return this;
+	}
+    });
 
 
+
+    templateLoader.on("load:templates", function () {
+	new LibraryView();
+    });
+
+    return Market;
+})(Market || {});
