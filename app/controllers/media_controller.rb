2c6063950d8b0513ab44bd435264402869326313
Maximilian <felgenh3@gmail.com>
2014-10-31 16:12:07 -0500

    Changed MediaController to add route
    When someone goes to route /media it gets routed the same way as
    /users/:user_id/media, so the controller needs to be robust enough
    to handle a nil parameter

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,7 +1,7 @@
 class MediaController < ApplicationController
-
   def index
     user_id = params[:user_id]
-    render json: User.find(user_id).media.as_json
+    render json: User.find(user_id).media.as_json unless user_id.nil?
+    render json: Medium.all
   end
 end
