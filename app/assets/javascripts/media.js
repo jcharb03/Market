25fb5b9ed7aee0103c3e888e86f30a2865458010
Maximilian <felgenh3@gmail.com>
2014-10-31 16:04:27 -0500

    Namespaced media model in client with other things
    refactored get glyph for media type
    
    As well as removed MediaCellView, tried talking it to soon.

--- app/assets/javascripts/media.js
+++ app/assets/javascripts/media.js
@@ -1,67 +1,61 @@
-var Media = Backbone.Model.extend({
-    defaults: {
-	title: "",
-	author: "",
-	type: "",
-	aux: "",
-	created: "",
-	owner: -1;
-    }
-});
+var Market = (function (Market) {
+    //Bind the model
 
-
-
-var MediaCellView = Backbone.Model.extend({
-
-    initialize: function () {
-	this.template = _.template($("media-cell-template").html());
-	this.render();
-    }
+    Market.Model = Market.Model || {};
     
-    render: function() {
-	var data = _.chain(this.model.attributes())
-	    .clone()
-	    .tap(addGlph)
-	    .value();
-
-
-	this.$el.html(this.template(data));
-	return this;
-	    
-	function addGlyph(data) {
-	    data.glyph = this.glyphiconForMedia();
-	};
-    }
-
-    glyphiconForMedia: function () {
-	var type, elem, glyphType;
-	type = this.model.get("type");
-	elem = $('<span></span>'); 
-
-	elem.addClass('glyphicon');
-
-	switch ( type ) {
-	case 'music':
-	    glyphType = "glyphicon-music";
-	    break;
-
-	case 'movie':
-	    glyphType = "glyphicon-film";
-	    break;
-
-	case 'book':
-	    glyphType = "glyphicon-book";
-	    break;
-
-	case 'game':
-	    glyphType = "glyphicon-tower";
-	    break;
-
-
-	default:
-	    glyphType = "glyphicon-ban-circle";
+    Market.Model.Medium = Backbone.Model.extend({
+	defaults: {
+	    title: "",
+	    author: "",
+	    type: "",
+	    aux: "",
+	    created: "",
+	    owner: -1pp
 	}
+    });
+    
+    Market.Model.Media = Backbone.Collection.extend({
+	model: Medium
+    });
+
+
+    //Bind the Media Vies
+    var MediumDetailView = Backbone.View.extend({
+	initialize: function() {
+	    this.template = _.template($("#media-template").html());
+	    this.render();
+	}
+	
+	render : function () {
+	    this.$el.html(this.template());
+	}
+	
+    });
+
+    
+    
+    return Market;
+
+
+    function getGlyph(type) {
+	var elem, glyphType;
+	const defaultGlyphType = "glyphicon-ban-circle";
+
+	glyphType = ({
+	    music: "glyphicon-music",
+	    movie: "glyphicon-film",
+	    book:  "glyphicon-book",
+	    game:  "glyphicon-tower"
+	}[type]) || defaultGlyphType;
+	
+	elem = $("<span></span>");
+	elem.addClass("glyphicon");
 	elem.addClass(glyphType);
+	
 	return elem;
     }
-});
+
+})(Market || {});
+
+
+
