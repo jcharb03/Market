0ff76bd35cc14c22da103d4f4cf6a9f6c73d0f10
Maximilian <felgenh3@gmail.com>
2014-10-31 04:15:02 -0500

    Fixed a bug with the representation of a user
    The name was showing the ruby representation for the object of the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def getUsername
-    @userName = User.all.first
+    @userName = User.all.first.name
   end
 end
