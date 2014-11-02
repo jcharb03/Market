1a8ecfe030c9300a4ebf7f82720e29402172cdc2
Maximilian <felgenh3@gmail.com>
2014-11-01 19:39:31 -0500

    Added route for /media/:id

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -3,13 +3,31 @@ var Market = (function (Market) {
     Market.Router = Backbone.Router.extend({
 	routes: {
 	    "" : "home",
-	    "add" : "addMedium"
+	    "add" : "addMedium",
+	    "media/:id": "showMedium"
 	},
 
 	addMedium: function() {
 	    console.log("Loading add Medium");
 	    $("#content").empty();
 	    new Market.Views.AddMediumView();
+	},
+
+	showMedium: function(id) {
+	    console.log("Routed show");
+	    console.log("id: " + id);
+	    var medium = new Market.Model.Medium({id: id});
+	    medium.fetch({
+		success:function(medium){
+		    console.log(medium);
+		    console.log("Got medium");
+		    $("#content").empty();
+		    new Market.Views.MediumDetailView({model: medium});
+		},
+		error:function() {
+		    console.log("Error");
+		}
+	    });
 	},
 
 	home: function() {
