b221106b56a1189b26b4d4b62af8cc53eb8a8417
Maximilian <felgenh3@gmail.com>
2014-11-10 17:22:31 -0600

    Added login view and authentication methods
    Added authentication and login view
    Added before filter to ensure authentication happened

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def getUser
+    if session[:id]
+      @user = User.find_by_id(session[:id])
+    else
+      redirect_to '/login'
+    end
+  end
+
   def getUsername
     @userName = User.all.first.name
   end
