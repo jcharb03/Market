525b0bb1160a33a9dafe53223f53787b16eb752e
Maximilian <felgenh3@gmail.com>
2014-11-13 17:15:35 -0600

    Added certain parameters for searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,14 @@ class SearchController < ApplicationController
 
   def create
     kind = params[:kind]
-    titleMatching = params[:title]
-
-
-    render json: [], status: 100
+    title = params[:title]
+    
+    content = Medium.all
+    
+    content = content.withKind kind if kind
+    content = content.withTitleMatching title if title
+    
+    render json: content.as_json, status: 200
   end
 
 end
