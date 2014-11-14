56a29efe156f52300a338d6407a47a966f628618
Maximilian <felgenh3@gmail.com>
2014-11-13 18:18:24 -0600

    Added recent flag in searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,11 +4,13 @@ class SearchController < ApplicationController
   def create
     kind = params[:kind]
     title = params[:title]
-    
+    recent = params[:recent]
+
     content = Medium.all
     
     content = content.withKind kind if kind
     content = content.withTitleMatching title if title
+    content = content.uploadedRecently if recent
     
     render json: content.as_json, status: 200
   end
