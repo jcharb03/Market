f1f51adff658ad086c9ef0ad08f7c1f1ba133a93
Maximilian <felgenh3@gmail.com>
2014-11-20 16:18:55 -0600

    Changed Factory to FactoryGirl

--- spec/models/medium_spec.rb
+++ spec/models/medium_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe "Medium" do
   it "has a valid factory" do
-    Factory.create(:medium).should be_valid
+    FactoryGirl.create(:medium).should be_valid
   end
-	it "is invalid without a title" do
-    Factory.build(:medium, title: nil).should_not be_valid
+  it "is invalid without a title" do
+    FactoryGirl.build(:medium, title: nil).should_not be_valid
   end
   it "is valid with only a title" do
-    Factory.build(:medium, title: "Halo").should be_valid
+    FactoryGirl.build(:medium, title: "Halo").should be_valid
   end
   it "is valid with all fields" do
-    Factory.build(:medium, title: "Halo", author: "343 Studio")
+    FactoryGirl.build(:medium, title: "Halo", author: "343 Studio")
   end
 end
