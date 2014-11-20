ff95cdfc27f8941fa8c490ba838ff9ed9046de62
Maximilian <felgenh3@gmail.com>
2014-11-20 16:28:35 -0600

    Fixed up authentication controller
    removed some print statements
    and protected against demeters law (sort of)

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,23 +1,16 @@
 class AuthenticationController < ApplicationController
 
   def authenticate
+    redirect_to action: 'login', failure: true if params[:session].nil?
+    
     email = params[:session][:email]
     password = params[:session][:password]
-    
-    puts session
-
     user = User.find_by(email: email)
-    puts user
+
     if user and user.authenticate password
       session[:id] = user.id
       redirect_to "/"
     else
-      if user.nil?
-        puts "User not found"
-      else
-        puts "Password not authentic"
-      end
-
       redirect_to action: 'login', failure: true
     end
   end
