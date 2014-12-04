483a24acde713de198d524b88963a858509558f6
felgenh3 <felgenh3@gmail.com>
2014-12-04 17:19:29 -0600

    Added stub for Password Reset Request Controller
    Changed password resets to work off of email instead of name
    Changed password reset to actually have passwords

--- app/controllers/password_reset_request_controller.rb
+++ app/controllers/password_reset_request_controller.rb
@@ -7,40 +7,29 @@ class PasswordResetRequestController < ApplicationController
 
   # Create the request and email for verification
   def create
-    name = params[:reset][:name]
-    user = User.find_by(name: name)
+    email = params[:reset][:email]
+    user = User.find_by(email: email)
 
     if user
-      @hash = Digest::SHA1.hexdigest("#{user.name}-#{DateTime.now.strftime("%M%S%L")}").to_s
+      @hash = Digest::SHA1.hexdigest("#{user.email}-#{DateTime.now.strftime("%M%S%L")}").to_s
       reset_request = PasswordResetRequest.new
       reset_request.user = user
       reset_request.link = @hash
       puts @hash
       if reset_request.save
         @fulfill_link = "/password_reset_request/#{@hash}"
+        #redirect_to '/'
       else
         render code: 500, text: "Unable to create code"
       end
     else
-      render code: 400, text: "Cannot find user: #{name}" 
+      render code: 400, text: "Cannot find user: #{email}" 
     end
   end
 
   # Redeem the reset code
   def show
-    @reset_request = PasswordResetRequest.find_by(link: params[:id])
-  end
 
-  def update
-    @reset_request = PasswordResetRequest.find_by(link: params[:id])
-    new_password = params[:reset][:new_password]
-    puts new_password
-    puts @reset_request
-    puts @reset_request.is_valid?
-    puts @reset_request.user
-    if new_password and not @reset_request.nil? and @reset_request.is_valid?
-      @reset_request.fulfill new_password
-      redirect_to "/login"
-    end
   end
+  
 end
---
+++ app/helpers/password_reset_request_helper.rb
@@ -0,0 +1,2 @@
+module PasswordResetRequestHelper
+end
